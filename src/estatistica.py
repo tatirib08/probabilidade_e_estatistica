378aa5ffa8fb1e0943fec16d9a0c89a8f194848d
Alan <alanvan3791@gmail.com>
2024-04-21 19:36:03 -0300

    deixando o codigo mais legivel (espero)

--- src/estatistica.py
+++ src/estatistica.py
@@ -1,54 +1,113 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
-from scipy.stats import pearsonr, spearmanr, kendalltau
-import csv 
+from scipy.stats import pearsonr, spearmanr
 import statsmodels.formula.api as smf 
 import seaborn as sns
 
 dados = pd.read_csv("src/dados/fa_casoshumanos_1994-2023_editado.csv")
-dados_correlacao = pd.read_csv("src/dados/fa_casoshumanos_1994-2023_correlacao.csv")
 dados_obitos_por_idade = pd.read_excel("src/dados/FrequenciaIdade.xlsx")
-#histograma casos por região
+
+'''
+    HISTOGRAMA CASOS POR REGIÃO
+'''
 histograma_casos_por_regiao = plt.figure("histograma_casos_por_regiao")
+''''''
 casos_por_regiao = dados.groupby(by="MACRORREG_LPI")["MACRORREG_LPI"]
 casos_por_regiao.hist(bins=2)
-#poligono de frequencia casos por região
+
+''' poligono de frequencia ''' 
 casos_por_regiao.value_counts().plot()
 
-# IDADE
+''' print '''
+print(f'\n\nCASOS POR REGIÃO: \n{casos_por_regiao.value_counts()}\n\n')
+''''''
+
+
+'''
+    HISTOGRAMA CASOS POR IDADE
+'''
 histograma_casos_por_idade = plt.figure("histograma_casos_por_idade")
-casos_por_idade = dados_correlacao.groupby(by="IDADE")["IDADE"]
+''''''
+casos_por_idade = dados.groupby(by="IDADE")["IDADE"]
 casos_por_idade.hist(bins=1)
-#obitos é outro dataframe 
-obitos = dados_correlacao.loc[dados_correlacao['OBITO'] == "SIM"]
-# print(obitos.to_string())
+''''''
 
+'''
+    HISTOGRAMA CASOS POR ANO
+'''
 histograma_casos_por_ano = plt.figure("histograma_casos_por_ano")
-casos_por_ano = dados.groupby(by="ANO_IS")["ANO_IS"]
-casos_por_ano.hist(bins=2)
-#poligono de frequencia casos por ano
-casos_por_ano.value_counts().plot()
+agrupado_por_ano = dados.groupby(by="ANO_IS")
+agrupado_por_ano["ANO_IS"].hist(bins=2)
+''' poligono de frequencia ''' 
+agrupado_por_ano["ANO_IS"].value_counts().plot()
+''''''
 
-plt.figure("quartis").show()
 
-media_casos_por_ano = casos_por_ano.count().mean()
-moda_casos_por_ano = casos_por_ano.count().mode()
-mediana_casos_por_ano = casos_por_ano.count().median()
-curtose_casos_por_ano = casos_por_ano.count().kurtosis()
-variancia_casos_por_ano = casos_por_ano.count().var()
-desvio_padrao_casos_por_ano = casos_por_ano.count().std()
+'''
+    ANÁLISE CASOS POR ANO
+'''
+df_casos_por_ano = agrupado_por_ano.size().reset_index(name="CASOS")
+coluna_casos = df_casos_por_ano['CASOS']
+''' média '''
+media_casos_por_ano = coluna_casos.mean()
+''' moda '''
+moda_casos_por_ano = coluna_casos.mode()
+''' mediana '''
+mediana_casos_por_ano = coluna_casos.median()
+''' curtose '''
+curtose_casos_por_ano = coluna_casos.kurtosis()
+''' variância '''
+variancia_casos_por_ano = coluna_casos.var()
+''' desvio padrão '''
+desvio_padrao_casos_por_ano = coluna_casos.std()
+''' quartis '''
+quartis_casos_por_ano = coluna_casos.quantile([0.25, 0.75])
+''' maior valor '''
+linha_maior_valor_casos_por_ano = df_casos_por_ano.iloc[coluna_casos.idxmax()]
+maior_valor_casos_por_ano = linha_maior_valor_casos_por_ano['CASOS']
+ano_maior_valor_casos_por_ano = linha_maior_valor_casos_por_ano['ANO_IS']
+''' menor valor '''
+linha_menor_valor_casos_por_ano = df_casos_por_ano.iloc[coluna_casos.idxmin()]
+menor_valor_casos_por_ano = linha_menor_valor_casos_por_ano['CASOS']
+ano_menor_valor_casos_por_ano = linha_menor_valor_casos_por_ano['ANO_IS']
+'''print'''
+print(f'\n\nANÁLISE CASOS POR ANO:')
+print(f'- Média -> {media_casos_por_ano}')
+print(f'- Moda -> {moda_casos_por_ano.to_list()}')
+print(f'- Mediana (Percentil 50) -> {mediana_casos_por_ano}')
+print(f'- Curtose -> {curtose_casos_por_ano}')
+print(f'- Variância -> {variancia_casos_por_ano}')
+print(f'- Desvio Padrão -> {desvio_padrao_casos_por_ano}')
+print(f'- Primeiro Quartil (Percentil 25) -> {quartis_casos_por_ano[0.25]}')
+print(f'- Terceiro Quartil (Percentil 75) -> {quartis_casos_por_ano[0.75]}')
+print(f'- Maior Quantidade de Casos -> {maior_valor_casos_por_ano}, ano {ano_maior_valor_casos_por_ano}')
+print(f'- Menor Quantidade de Casos -> {menor_valor_casos_por_ano}, ano {ano_menor_valor_casos_por_ano}')
+print("\n")
 
-print(f'Média -> {media_casos_por_ano}')
-print(f'Moda -> {moda_casos_por_ano.to_list()}')
-print(f'Mediana -> {mediana_casos_por_ano}')
-print(f'Curtose -> {curtose_casos_por_ano}')
-print(f'Variância -> {variancia_casos_por_ano}')
-print(f'Desvio Padrão -> {desvio_padrao_casos_por_ano}')
+''' box plot '''
+box_plot_df = df_casos_por_ano
+iqr = box_plot_df['CASOS'].quantile(0.75) - box_plot_df['CASOS'].quantile(0.25)
+box_plot_casos_por_ano = plt.figure("boxplot_casos_por_ano")
+box_plot_casos_por_ano.gca().set_ylim([0, box_plot_df['CASOS'].quantile(0.75) + 1.5 * iqr])
+_, bp = box_plot_df.boxplot(column =['CASOS'], grid = False, return_type='both')
+''''''
 
-quartis_casos_por_ano = casos_por_ano.count().quantile([.25, .5, .75]) 
 
-quartis_casos_por_ano.plot.bar(color=['#2876EB', '#5728EB', '#2838EB'])
+# outliers = [flier.get_ydata() for flier in bp["fliers"]]
+# boxes = [box.get_ydata() for box in bp["boxes"]]
+# medians = [median.get_ydata() for median in bp["medians"]]
+# whiskers = [whiskers.get_ydata() for whiskers in bp["whiskers"]]
+
+# print(f"outlier -> {outliers}")
+# print(f"box -> {boxes}")
+# print(f"median -> {medians}")
+# print(f"whisker -> {whiskers}")
+
+# print(f'stats -> {box_plot_df["COUNT"].describe()}')
+
+# quartis_casos_por_ano = casos_por_ano.count().quantile([.25, .5, .75]) 
+
+# quartis_casos_por_ano.plot.bar(color=['#2876EB', '#5728EB', '#2838EB'])
 
 '''
     CORRELAÇÃO LINEAR
