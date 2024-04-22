0a9a8c15ebaff7d4492662d00ab8ec540442cf8d
Alan <alanvan3791@gmail.com>
2024-04-22 01:55:59 -0300

    removido dados que não estavam sendo utilizados; todos os gráficos agora tem nome nos eixos e titulo; adicionada amplitude e amplitude interquartil

--- src/estatistica.py
+++ src/estatistica.py
@@ -4,13 +4,16 @@ from scipy.stats import pearsonr, spearmanr
 import statsmodels.formula.api as smf 
 import seaborn as sns
 
-dados = pd.read_csv("src/dados/fa_casoshumanos_1994-2023_editado.csv")
-dados_obitos_por_idade = pd.read_excel("src/dados/FrequenciaIdade.xlsx")
+dados = pd.read_csv("src/dados/colunas_filtradas.csv")
+dados_obitos_por_idade = pd.read_excel("src/dados/frequencia_obitos_por_idade.xlsx")
 
 '''
     HISTOGRAMA CASOS POR REGIÃO
 '''
 histograma_casos_por_regiao = plt.figure("histograma_casos_por_regiao")
+plt.title("Casos agrupados por região")
+plt.xlabel("Região")
+plt.ylabel("Casos")
 ''''''
 casos_por_regiao = dados.groupby(by="MACRORREG_LPI")["MACRORREG_LPI"]
 casos_por_regiao.hist(bins=2)
@@ -27,25 +30,20 @@ print(f'\n\nCASOS POR REGIÃO: \n{casos_por_regiao.value_counts()}\n\n')
     HISTOGRAMA CASOS POR IDADE
 '''
 histograma_casos_por_idade = plt.figure("histograma_casos_por_idade")
+plt.title("Casos agrupados por idade")
+plt.xlabel("Idade")
+plt.ylabel("Casos")
 ''''''
 casos_por_idade = dados.groupby(by="IDADE")["IDADE"]
 casos_por_idade.hist(bins=1)
 ''''''
 
 '''
-    HISTOGRAMA CASOS POR ANO
-'''
-histograma_casos_por_ano = plt.figure("histograma_casos_por_ano")
-agrupado_por_ano = dados.groupby(by="ANO_IS")
-agrupado_por_ano["ANO_IS"].hist(bins=2)
-''' poligono de frequencia ''' 
-agrupado_por_ano["ANO_IS"].value_counts().plot()
-''''''
-
-
-'''
     ANÁLISE CASOS POR ANO
 '''
+agrupado_por_ano = dados.groupby(by="ANO_IS")
+histograma_casos_por_ano = plt.figure("histograma_casos_por_ano_completo")
+
 df_casos_por_ano = agrupado_por_ano.size().reset_index(name="CASOS")
 coluna_casos = df_casos_por_ano['CASOS']
 ''' média '''
@@ -70,6 +68,10 @@ ano_maior_valor_casos_por_ano = linha_maior_valor_casos_por_ano['ANO_IS']
 linha_menor_valor_casos_por_ano = df_casos_por_ano.iloc[coluna_casos.idxmin()]
 menor_valor_casos_por_ano = linha_menor_valor_casos_por_ano['CASOS']
 ano_menor_valor_casos_por_ano = linha_menor_valor_casos_por_ano['ANO_IS']
+''' amplitude '''
+amplitude_casos_por_ano = maior_valor_casos_por_ano - menor_valor_casos_por_ano
+''' amplitude interquartil '''
+amplitude_interquartil_casos_por_ano = quartis_casos_por_ano[0.75] - quartis_casos_por_ano[0.25]
 '''print'''
 print(f'\n\nANÁLISE CASOS POR ANO:')
 print(f'- Média -> {media_casos_por_ano}')
@@ -82,32 +84,54 @@ print(f'- Primeiro Quartil (Percentil 25) -> {quartis_casos_por_ano[0.25]}')
 print(f'- Terceiro Quartil (Percentil 75) -> {quartis_casos_por_ano[0.75]}')
 print(f'- Maior Quantidade de Casos -> {maior_valor_casos_por_ano}, ano {ano_maior_valor_casos_por_ano}')
 print(f'- Menor Quantidade de Casos -> {menor_valor_casos_por_ano}, ano {ano_menor_valor_casos_por_ano}')
+print(f'- Amplitude de Casos -> {amplitude_casos_por_ano}')
+print(f'- Amplitude Interquartil de Casos -> {amplitude_interquartil_casos_por_ano}')
+
 print("\n")
 
-''' box plot '''
-box_plot_df = df_casos_por_ano
-iqr = box_plot_df['CASOS'].quantile(0.75) - box_plot_df['CASOS'].quantile(0.25)
-box_plot_casos_por_ano = plt.figure("boxplot_casos_por_ano")
-box_plot_casos_por_ano.gca().set_ylim([0, box_plot_df['CASOS'].quantile(0.75) + 1.5 * iqr])
-_, bp = box_plot_df.boxplot(column =['CASOS'], grid = False, return_type='both')
+
+'''
+    HISTOGRAMA CASOS POR ANO
+'''
+histograma_casos_por_ano = plt.figure("histograma_casos_por_ano_completo")
+plt.title("Casos agrupados por ano")
+plt.xlabel("Ano")
+plt.ylabel("Casos")
+agrupado_por_ano["ANO_IS"].hist(bins=1)
+
+histograma_casos_por_ano = plt.figure("histograma_casos_por_ano_y_limitado")
+histograma_casos_por_ano.gca().set_ylim([0, quartis_casos_por_ano[0.75] + 1.5 * amplitude_interquartil_casos_por_ano])
+plt.title("Casos agrupados por ano com eixo Y limitado")
+plt.xlabel("Ano")
+plt.ylabel("Casos")
+agrupado_por_ano["ANO_IS"].hist(bins=1)
 ''''''
 
+'''
+    BOX PLOT CASOS POR ANO 
+'''
+''' completo, com eixo y original'''
+box_plot_casos_por_ano = plt.figure("boxplot_casos_por_ano_completo")
+plt.title("Casos agrupados por ano")
+plt.ylabel("Casos")
+_, bp = df_casos_por_ano.boxplot(column =['CASOS'], whis=1.5, grid = False, return_type='both')
+''' eixo y limitado a somente o box plot'''
+box_plot_casos_por_ano_limitado = plt.figure("boxplot_casos_por_ano_limitado_eixo_y")
+plt.title("Casos agrupados por ano com eixo Y limitado")
+plt.ylabel("Casos")
+box_plot_casos_por_ano_limitado.gca().set_ylim([0, quartis_casos_por_ano[0.75] + 1.5 * amplitude_interquartil_casos_por_ano])
+_, bp = df_casos_por_ano.boxplot(column =['CASOS'], whis=1.5, grid = False, return_type='both')
+'''print'''
+print(f'\n\n DADOS BOX PLOT CASOS POR ANO:')
+outliers = [flier.get_ydata() for flier in bp["fliers"]]
+print(f'- Valores discrepantes (outliers) -> {outliers[0]}')
+limites = [whiskers.get_ydata() for whiskers in bp["whiskers"]]
+limite_superior = limites[1][1]
+print(f'- Limite superior -> {limite_superior}')
 
-# outliers = [flier.get_ydata() for flier in bp["fliers"]]
-# boxes = [box.get_ydata() for box in bp["boxes"]]
-# medians = [median.get_ydata() for median in bp["medians"]]
-# whiskers = [whiskers.get_ydata() for whiskers in bp["whiskers"]]
+print("\n")
+''''''
 
-# print(f"outlier -> {outliers}")
-# print(f"box -> {boxes}")
-# print(f"median -> {medians}")
-# print(f"whisker -> {whiskers}")
-
-# print(f'stats -> {box_plot_df["COUNT"].describe()}')
-
-# quartis_casos_por_ano = casos_por_ano.count().quantile([.25, .5, .75]) 
-
-# quartis_casos_por_ano.plot.bar(color=['#2876EB', '#5728EB', '#2838EB'])
 
 '''
     CORRELAÇÃO LINEAR
@@ -116,12 +140,14 @@ _, bp = box_plot_df.boxplot(column =['CASOS'], grid = False, return_type='both')
 # diagrama de dispersão (scatter plot):
 dispersaoObitosIdade = plt.figure("diagrama_dispersão_idade_óbitos")
 plt.scatter(dados_obitos_por_idade['IDADE'], dados_obitos_por_idade['OBITOS'], color='r')
+plt.title("Óbitos agrupados por idade")
 plt.xlabel("Idade")
 plt.ylabel("Óbitos")
 
 dispersaoCasosPorAno = plt.figure("dispersão_casos_por_ano")
 dfCasosPorAno = dados.groupby(["ANO_IS"]).size().reset_index(name="COUNT")
 plt.scatter(dfCasosPorAno['ANO_IS'], dfCasosPorAno['COUNT'], color='r')
+plt.title("Casos agrupados por ano")
 plt.xlabel("Ano")
 plt.ylabel("Casos")
 
@@ -143,9 +169,6 @@ coef_spearman, p_valor = spearmanr(dados_obitos_por_idade['IDADE'], dados_obitos
 ''' 
     Coeficiente de correlação de Kendall
 '''
-
-print(dados_obitos_por_idade["IDADE"].corr(dados_obitos_por_idade["OBITOS"]))
-
 print(f"COEFICIENTE DE PEARSON:  {coef_pearson}")
 # coef de pearson output = -0.00675470470148104
 print(f"COEFICIENTE DE SPEARMAN:  {coef_spearman}")
@@ -159,12 +182,12 @@ print(f"COEFICIENTE DE SPEARMAN:  {coef_spearman}")
     Regressão linear múltipla: um pra muitos
 '''
 regression = smf.ols('OBITOS ~ IDADE', data=dados_obitos_por_idade)
-regression.fit().summary()
+print(regression.fit().summary())
 plt.figure("regressão_óbitos_por_idade")
 sns.regplot(x='IDADE', y='OBITOS', data=dados_obitos_por_idade)
 
 regression2 = smf.ols('COUNT ~ ANO_IS', data=dfCasosPorAno)
-regression2.fit().summary()
+print(regression2.fit().summary())
 plt.figure("regressão_casos_por_ano")
 sns.regplot(x='ANO_IS', y='COUNT', data=dfCasosPorAno)
 
