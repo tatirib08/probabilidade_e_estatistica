5dc5a356cf3836dd138d0141a82ff254b5c3ba12
Alan <alanvan3791@gmail.com>
2024-05-27 17:29:00 -0300

    codigo teste t por sexo limpo

---
+++ src/código/teste_t_sexo.R
@@ -0,0 +1,18 @@
+library(dplyr)
+
+Dados <-read.csv('dados/original.csv', sep=';', encoding = 'UTF-8')
+
+View(Dados) 
+
+municipios <- Dados %>%
+  group_by(MUN_LPI, UF_LPI, SEXO) %>%
+  summarise(frequencia = n()) %>%
+  ungroup()
+
+View(municipios)
+
+#variância desigual
+t.test(municipios$frequencia~municipios$SEXO)    
+
+#variância igual
+t.test(municipios$frequencia~municipios$SEXO, var.equal=T) 
